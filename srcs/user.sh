fef66b40118c6e2a1321d59f133b6dc19a871991
mokellat <fsociety10@gmail.com>
2020-12-04 10:49:36 +0100

    done

--- srcs/user.sh
+++ srcs/user.sh
@@ -5,7 +5,7 @@ service nginx start
 mysql -uroot -proot -e "CREATE USER 'mokellat'@'localhost' IDENTIFIED BY '123456'"
 mysql -uroot -proot -e "CREATE DATABASE wordpress"
 mysql -uroot -proot -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'mokellat'@'localhost'"
-mysql wordpress -u root --password=  < wordpress.sql
+mysql wordpress --user=mokellat  --password=123456 < wordpress.sql
 mysql -uroot -proot -e "FLUSH PRIVILEGES"
 
 bash
